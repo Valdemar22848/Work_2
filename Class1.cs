4097a763f90c9a2f5f7ae9a7e978111694f1dc0d
Valdemar22848 <122788990+Valdemar22848@users.noreply.github.com>
2024-02-22 19:07:30 +0300

    Работа2

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Work2;
+public class Class1
+{
+    public void Main ()
+    {
+        int y = int.Parse(Console.ReadLine());
+        int x = 10 - y * 2;
+        Console.WriteLine(x);
+    }
+}
