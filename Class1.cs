f78daf29951fb3cc8fcaf15bd219780e5f65ca97
Valdemar22848 <122788990+Valdemar22848@users.noreply.github.com>
2024-02-22 19:10:22 +0300

    Update Class1.cs (уменьшил в 2 раза)
    Готово к работе

--- Class1.cs
+++ Class1.cs
@@ -4,7 +4,7 @@ public class Class1
     public void Main ()
     {
         int y = int.Parse(Console.ReadLine());
-        int x = 10 - y * 2;
+        int x = (10 - y * 2) / 2;
         Console.WriteLine(x);
     }
 }
